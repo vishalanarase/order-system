fc27eccf59fe316b7576cf42dd2b914a64f12540
Vishal Anarase <iamvishalanarase@gmail.com>
2025-09-14 11:02:50 +0530

    Update main to init and read
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/vishalanarase/order-system/pkg/config"
+	"github.com/vishalanarase/order-system/pkg/logger"
+)
 
 func main() {
 	fmt.Println("Hello!")
+	// Load configuration
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+
+	// Initialize logger
+	if err := logger.InitLogger(cfg.Environment); err != nil {
+		log.Fatal("Failed to initialize logger:", err)
+	}
+	defer logger.Sync()
+
+	logger.Log.Info("Starting Order Processing System")
+
+	logger.Log.Sugar().Infof("config: %+v", cfg)
 }
