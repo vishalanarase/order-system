136244adede1eb377534def6c37dd03eaa6b740a
Vishal Anarase <iamvishalanarase@gmail.com>
2025-09-16 19:04:14 +0530

    Update order api
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -2,13 +2,14 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/vishalanarase/order-system/internal/services"
 	"go.uber.org/zap"
 )
 
-func RegisterRoutes(log *zap.Logger, router *gin.Engine) {
+func RegisterRoutes(log *zap.Logger, router *gin.Engine, orderService *services.OrderService) {
 	healthHandler := NewHealthHandler(log)
 
-	orderHandler := NewOrderHandler(log)
+	orderHandler := NewOrderHandler(log, orderService)
 
 	// Health check endpoint
 	router.GET("/health", healthHandler.HealthCheck)
