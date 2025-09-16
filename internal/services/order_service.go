1285887b5c5264e902c76b83cfdd7dc2e272c716
Vishal Anarase <iamvishalanarase@gmail.com>
2025-09-16 19:05:47 +0530

    Update order service
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- internal/services/order_service.go
+++ internal/services/order_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"time"
+
 	"github.com/vishalanarase/order-system/internal/kafka"
 	"github.com/vishalanarase/order-system/internal/models"
 	"go.uber.org/zap"
@@ -20,5 +22,25 @@ func NewOrderService(log *zap.Logger, producer *kafka.Producer) *OrderService {
 
 func (s *OrderService) CreateOrder(order models.Order) error {
 	s.log.Info("Service: CreateOrder called")
+	orderEvent := models.OrderEvent{
+		Type:      "ORDER_CREATED",
+		Order:     order,
+		Timestamp: time.Now(),
+		Metadata: models.Metadata{
+			CorrelationID: order.ID,
+			Source:        "order-service",
+		},
+	}
+
+	if err := s.producer.ProduceMessage(order.ID, orderEvent); err != nil {
+		return err
+	}
+
+	s.log.Info("Order created and sent to Kafka",
+		zap.String("order_id", order.ID),
+		zap.String("user_id", order.UserID),
+		zap.Float64("total_amount", order.TotalAmount),
+	)
+
 	return nil
 }
